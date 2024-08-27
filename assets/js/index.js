34dca72854b8bb87202a12226d7f1ee86381cfa2
Emiliano Rotta <emilianorottaf@gmail.com>
2024-08-26 21:05:11 -0300

    tipos de datos

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+//Tipo de datos.
+
+// Numbers
+
+var numero = 158
+var numeroDos = 15.25
+var noEsUnNumero = NaN
+
+
+//String
+
+var string = "hola Mundo"
+var otroString = 'otro string'
+
+//Booleanos
+
+var verdadero = true
+var falso = false
+
+var fecha = new Date()
+
+//array    
+var array = ["perros", 5, [],{}]
+
+//objetos
+var objetos = {nombre: "Emiliano", apellido: "Rotta"}
+
+//Actividad: Quiero un alert que diga. Emiliano tiene 5 perros
+
+// alert(objetos.nombre + " tiene "+ array[1] + " " + array[0] )
+
+// Diferencia entre Concatenacion e interpolacion
+//concatenar:
+alert(objetos.nombre + " tiene " + array[1] + " " + array[0] )
+//interpolacion
+alert(`${objetos.nombre} tiene ${array[1]} ${array[0]}`)
