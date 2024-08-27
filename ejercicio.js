211d68ff78d42f9f44b38451885c8f675a651947
Emiliano Rotta <emilianorottaf@gmail.com>
2024-08-26 22:15:00 -0300

    ejercicio

---
+++ ejercicio.js
@@ -0,0 +1,15 @@
+// Instrucciones:
+// Declara una variable strNumber con el valor "1234".
+// Declara una variable num con el valor 2024.
+// Declara una variable dateStr con el valor "2024-12-31".
+// Declara una variable isTrue con el valor true.
+// Declara una variable isFalse con el valor false.
+// Tareas:
+// Convierte strNumber en un número entero y muestra el resultado en la consola.
+// Convierte num en una cadena de texto y muestra el resultado en la consola.
+// Convierte dateStr en un objeto de tipo Date y muestra el resultado en la consola.
+// Convierte el objeto Date anterior en milisegundos desde el 1 de enero de 1970 y muestra el resultado en la consola.
+// Convierte isTrue y isFalse en números y muestra ambos resultados en la consola.
+// Convierte un número en un booleano y muestra el resultado en la consola.
+
+// https://www.w3schools.com/js/js_type_conversion.asp
